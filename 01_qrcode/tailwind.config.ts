535ad22a7ebc4380ea1c928d3281fef2b165ffe5
MinhTam-Infodation <tam.minhhoang@infodation.vn>
2025-01-06 22:13:22 +0700

    Comit Update

--- 01_qrcode/tailwind.config.ts
+++ 01_qrcode/tailwind.config.ts
@@ -11,6 +11,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        state_900: "#1F314F",
+        state_500: "#68778D",
+        state_300: "#D5E1EF",
+        white: "#FFFFFF",
       },
     },
   },
